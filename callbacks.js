15ed49b57b72fb99152c05da5826388b56bb1124
Ben Rothman <croquetpro@gmail.com>
2015-11-27 14:02:19 -0800

    finished callbacks.js

--- callbacks.js
+++ callbacks.js
@@ -43,6 +43,48 @@ function addNumbers(sum, numsLeft, completionCallback){
 //   console.log("Total Sum: " + sum);
 // });
 
-function absurdBubbleSort(){
-  
+function askIfGreaterThan(el1, el2, callback){
+  reader.question("Is " + el1 + " > " + el2 + "? ", function (answer){
+      if (answer === "yes" || answer === 'y') {
+        var bool = true;
+      } else {
+        var bool = false;
+      }
+  callback(bool);
+  });
 }
+
+function innerBubbleSortLoop(arr, i, madeAnySwaps, outerBubbleSortLoop){
+  if (i < arr.length - 1) {
+    askIfGreaterThan(arr[i], arr[i + 1], function(isGreaterThan) {
+      if (isGreaterThan === true) {
+        arr[i + 1] = [arr[i], arr[i]=arr[i+1]][0];
+        madeAnySwaps = true;
+      }
+      if (i === (arr.length - 2)) {
+        outerBubbleSortLoop(madeAnySwaps);
+      }
+      innerBubbleSortLoop(arr, i+1, madeAnySwaps, outerBubbleSortLoop);
+    });
+  }
+}
+
+function absurdBubbleSort(arr, sortCompletionCallback){
+
+  function outerBubbleSortLoop(madeAnySwaps){
+    if( madeAnySwaps === true){
+      innerBubbleSortLoop(arr, 0, false, outerBubbleSortLoop);
+    }
+    else{
+
+      sortCompletionCallback(arr);
+    }
+  }
+
+  outerBubbleSortLoop(true);
+}
+
+absurdBubbleSort([3, 2, 1, 4], function (arr) {
+  console.log("Sorted array: " + JSON.stringify(arr));
+  reader.close();
+});
